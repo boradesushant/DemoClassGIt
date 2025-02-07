95383ff2c683c2fff58a488784dfd3bf39aa951c
boradesushant <boradesushant50@gmail.com>
2025-02-07 23:12:47 +0530

    TC_003

---
+++ src/test/java/Com_Test/TC__003.java
@@ -0,0 +1,14 @@
+package Com_Test;
+
+import org.testng.annotations.Test;
+
+public class TC__003 {
+
+	@Test
+	public void TC_003 () {
+		
+		System.out.println("Test_003");
+		
+		System.out.println("Test_003");
+	}
+}
